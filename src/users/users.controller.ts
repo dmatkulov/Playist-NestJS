d7345fd063d818193f9c7d8ebf9b7c62ed0bd1e7
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-03-23 19:52:19 +0600

    HW#94 added logout for authorized users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Post,
   Req,
   UnprocessableEntityException,
@@ -12,6 +13,10 @@ import mongoose, { Model } from 'mongoose';
 import { RegisterUserDto } from './register-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { Roles } from '../decorators/roles.decorator';
+import { Role } from '../enums/role.enum';
+import { TokenAuthGuard } from '../auth/token-auth.guard';
+import { RolesGuard } from '../auth/roles-guard.guard';
 
 @Controller('users')
 export class UsersController {
@@ -46,5 +51,34 @@ export class UsersController {
   @Post('sessions')
   async login(@Req() req: Request) {
     return req.user;
+  }
+
+  @Roles(Role.User, Role.Admin)
+  @UseGuards(TokenAuthGuard, RolesGuard)
+  @Delete('sessions')
+  async logOut(@Req() req: Request) {
+    const headerValue = req.get('Authorization');
+    const successMessage = { message: 'Success' };
+
+    if (!headerValue) {
+      return successMessage;
+    }
+
+    const [_bearer, token] = headerValue.split(' ');
+
+    if (!token) {
+      return successMessage;
+    }
+
+    const user = await this.userModel.findOne({ token });
+
+    if (!user) {
+      return successMessage;
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return successMessage;
   }
 }
